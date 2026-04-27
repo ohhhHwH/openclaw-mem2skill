e66d632f9bf66e89f5f2bd9f4798d56b59d18f88
hyl <1729508169@qq.com>
2026-04-27 21:17:25 +0800

    update install bash

--- install.sh
+++ install.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
-git pull
+
+# MAX_RETRIES=3
+# attempt=0
+# while true; do
+#   if git pull; then
+#     echo "git pull 成功"
+#     break
+#   fi
+#   attempt=$((attempt+1))
+#   echo "git pull fail ${attempt} "
+#   if [ "$attempt" -ge "$MAX_RETRIES" ]; then
+#     echo "max try ${MAX_RETRIES} exit"
+#     exit 1
+#   fi
+#   sleep 2
+# done
+
 npm pack
 openclaw plugins uninstall memory2skill
 openclaw plugins install ./memory2skill-1.0.0.tgz
